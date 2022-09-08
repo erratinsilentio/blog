1ff6c87f61a32d2c13519dd8bf66f0f03c9d5c84
erratinsilentio <kacper.kondracki@wp.pl>
2022-09-08 20:40:39 +0200

    added giphy API

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,46 @@ const photoAction = (() => {
     nav_blog.classList.remove("chosenLi");
     nav_photos.classList.add("chosenLi");
 
-   
+    const container = document.createElement("div");
+    //container.classList.add("opacity");
+    container.classList.add("flex-column");
+    container.classList.add("change");
+    //container.style.outline = "2px solid white"
+    container.style.height = "60%";
+    container.style.width = "60%";
+    container.style.marginTop = "2%";
+    container.style.marginLeft = "5%";
+    container.style.padding = "5%";
+    right.appendChild(container);   
+
+    const photo = document.createElement("img");
+    photo.style.width = "90%";
+    photo.style.height = "90%";
+    photo.style.margin = "5px";
+    photo.style.display = "none";
+    container.appendChild(photo);
+
+
+    const getPhoto = () => {
+      fetch(
+        "https://api.giphy.com/v1/gifs/translate?api_key=AB1Q7N5UIzTk5LDFiN5WQkWvm9H3P7iL&s=cats",
+        { mode: "cors" }
+      ).then(function (response) {
+        return response.json();
+      })
+      .then(function(response){
+        photo.src = response.data.images.original.url
+        photo.style.display = "block";
+        photo.classList.add("height")
+      });
+    }
+
+    const button = document.createElement("button");
+    button.classList.add("kitty-button");
+    button.textContent = "MEOW!"
+    button.style.fontSize = "clamp(0.5rem, 1vw, 1.5rem)";
+    button.addEventListener('click', getPhoto);
+    container.appendChild(button);
   };
 
   return {
